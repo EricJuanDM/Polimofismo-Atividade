8ad9de5510a2cc318203380562968280d46b8a30
EricJuanDM <ericjuandm@gmail.com>
2025-09-24 20:01:47 -0300

    w
    w

--- Circulo.java
+++ Circulo.java
@@ -1,8 +1,12 @@
 public class Circulo extends Forma{
     double raio;
 
+    public Circulo(double raio){
+        this.raio = raio;
+    }
+
     @Override
     public double calcularArea(){
-        return 
+        return Math.PI * (raio * raio);
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
         Quadrado q1 = new Quadrado();
+        Retangulo r1 = new Retangulo();
+        Circulo c1 = new Circulo();
+
+        ArrayList<Forma> l1 = new ArrayList<>;
+
+        l1.add (q1);
+        l1.add (r1);
+        l1.add(c1);
+
+        for (Forma forma : l1) {
+            System.out.println(calcularArea);
+        }
 
     }
 }
